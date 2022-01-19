93252e176d5299bab80d6aa5f645ee3bc2a25ea7
hormese1236 <hormese.johnson@rapidvaluesolutions.com>
2022-01-19 10:49:19 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
-
-import './App.css';
-
-
+import "./App.css";
 
 function App() {
-
-  
   return (
-
-   <div id="header">
-   
+    <div id="header">
       <h1>weather</h1>
-       
-      
-       <style>{'body { background-color:black; }'}</style>
-       <div id="textbox">
-   
-        <label> <input  id="search" type='text' placeholder='search your city or airport' /> 
-    
- 
 
- </label> 
- </div>
-  </div>
+      <style>{"body { background-color:black; }"}</style>
+      <div id="textbox">
+        <label>
+          {" "}
+          <input
+            id="search"
+            type="text"
+            placeholder="search your city or airport"
+          />
+        </label>
+      </div>
+    </div>
   );
 }
 
-  
 export default App;
